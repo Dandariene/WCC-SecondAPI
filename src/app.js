1dc870a70b75584e52ca856148e7f36ed56d73b7
Dandariene <dandariene@hotmail.com>
2021-05-26 12:30:52 -0300

    criando a segunda API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const configexpress = require('./config/configExpress');
 const config = require('config');
+const instanciadb = require('./db');
 
-app = configexpress()
+(async () => {
+    try {
+        await instanciadb.sync({orce: true})
 
-app.listen(config.get('api.port'), () => {
-    console.log('Servidor Rodando!')
-});
+        app = configexpress()
+
+        app.listen(config.get('api.port'), () => {
+        console.log('Servidor Rodando!')
+        });
+    } catch (error) {
+        throw error;
+    };
+
+})();
+
+
